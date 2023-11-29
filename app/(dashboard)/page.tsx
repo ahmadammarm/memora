002942736455cda9ec572dd302dad7cd6160d60e
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2023-11-30 06:50:08 +0700

    update page and make prisma lib

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -2,6 +2,8 @@ import { Skeleton } from "@/components/ui/skeleton";
 import { wait } from "@/lib/wait";
 import { currentUser } from "@clerk/nextjs"
 import { Suspense } from "react";
+import prisma from '../../lib/prisma';
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 
 export default async function Home() {
   return (
@@ -38,9 +40,25 @@ function WelcomeMsgFallback(){
   return (
     <div className="flex w-full">
       <h1 className="text-4xl font-bold">
-        <Skeleton className="w-[150px] h-[36px]" />
+        <Skeleton className="w-[180px] h-[36px]" />
         <Skeleton className="w-[150px] h-[36px]" />
       </h1>
     </div>
   )
+}
+
+async function CollectionList() {
+  const user = await currentUser();
+  const collection = await prisma.collection.findMany({
+    where: {
+      userId: user?.id,
+    }
+  });
+
+  if(collection.length === 0){
+    <Alert>
+      <AlertTitle>There are No Collections Yet!</AlertTitle>
+      <AlertDescription>Create a Collection to Get Started!</AlertDescription>
+    </Alert>
+  }
 }
