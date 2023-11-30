95b8750bf0f333cee2aec23f0a54cda92e5ef039
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2023-11-30 07:41:44 +0700

    update form

--- components/CreateCollectionSheet.tsx
+++ components/CreateCollectionSheet.tsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle } from './ui/sheet';
+import { useForm } from 'react-hook-form';
+import { CreateCollectionSchemaType, createCollectionSchema} from '../schema/createCollection';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from './ui/form';
+import { Input } from './ui/input';
 
 interface Props {
     open: boolean;
@@ -7,6 +12,16 @@ interface Props {
 }
 
 function CreateCollectionSheet({open, onOpenChange}: Props) {
+    const form = useForm<CreateCollectionSchemaType>({
+        resolver: zodResolver(createCollectionSchema),
+        defaultValues: {},
+    })
+
+    const onSubmit = (data: CreateCollectionSchemaType) => {
+        console.log("Submitted", data)
+    }
+
+
   return (
     <Sheet open={open} onOpenChange={onOpenChange}>
         <SheetContent>
@@ -16,6 +31,24 @@ function CreateCollectionSheet({open, onOpenChange}: Props) {
                     Collections are a way to group your tasks. 
                 </SheetDescription>
             </SheetHeader>
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)}>
+                    <FormField
+                    control={form.control}
+                    name="name"
+                    render={({field}) => (
+                        <FormItem>
+                            <FormLabel>Name</FormLabel>
+                            <FormControl>
+                                <Input {...field} placeholder="Name your collection" />
+                            </FormControl>
+                            <FormDescription>Collection Name</FormDescription>
+                            <FormMessage/>
+                        </FormItem>
+                    )}
+                    />
+                </form>
+            </Form>
         </SheetContent>
     </Sheet>
   )
