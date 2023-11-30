b2df947aac04fd6df42ad2f9f7e00f9b9898ce6a
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2023-11-30 07:55:11 +0700

    update constants lib

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,15 +1,12 @@
 export enum CollectionColors {
-    sunset = "bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500",
-    ocean = "bg-gradient-to-r from-green-400 via-blue-500 to-purple-500",
-    forest = "bg-gradient-to-r from-green-400 via-green-500 to-green-600",
-    sky = "bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600",
-    fire = "bg-gradient-to-r from-yellow-400 via-red-500 to-yellow-500",
-    grass = "bg-gradient-to-r from-green-400 via-green-500 to-green-600",
-    water = "bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600",
-    earth = "bg-gradient-to-r from-yellow-400 via-yellow-500 to-yellow-600",
-    electric = "bg-gradient-to-r from-yellow-400 via-yellow-500 to-yellow-600",
-    ice = "bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600",
-    fighting = "bg-gradient-to-r from-red-400 via-red-500 to-red-600",
+    Sunset = "bg-gradient-to-r from-red-500 to-orange-500",
+    Poppy = "bg-gradient-to-r from-rose-400 to-red-500",
+    Rosebud = "bg-gradient-to-r from-violet-500 to-purple-500",
+    Snowflake = "bg-gradient-to-r from-indigo-400 to-cyan-400",
+    Candy = "bg-gradient-to-r from-yellow-400 via-pink-500 to-red-500",
+    Firtree = "bg-gradient-to-r from-emerald-500 to-emerald-900",
+    Metal = "bg-gradient-to-r from-slate-500 to-slate-800",
+    Powder = "bg-gradient-to-r from-violet-200 to-pink-200",
 }
 
-export type CollectionColor = keyof typeof CollectionColors
+export type CollectionColor = keyof typeof CollectionColors;
