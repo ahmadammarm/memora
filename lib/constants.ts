51fa13195ddba34f6e18cd8a06fb99fef3f74ce6
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2023-11-30 08:03:44 +0700

    update constants lib

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,12 +1,11 @@
 export enum CollectionColors {
-    Sunset = "bg-gradient-to-r from-red-500 to-orange-500",
-    Poppy = "bg-gradient-to-r from-rose-400 to-red-500",
-    Rosebud = "bg-gradient-to-r from-violet-500 to-purple-500",
-    Snowflake = "bg-gradient-to-r from-indigo-400 to-cyan-400",
-    Candy = "bg-gradient-to-r from-yellow-400 via-pink-500 to-red-500",
-    Firtree = "bg-gradient-to-r from-emerald-500 to-emerald-900",
-    Metal = "bg-gradient-to-r from-slate-500 to-slate-800",
-    Powder = "bg-gradient-to-r from-violet-200 to-pink-200",
+    Fire = "bg-gradient-to-r from-yellow-500 to-red-600",
+    Forest = "bg-gradient-to-r from-green-500 to-green-800",
+    Lavender = "bg-gradient-to-r from-purple-400 to-pink-400",
+    Sky = "bg-gradient-to-r from-cyan-500 to-cyan-900",
+    Earth = "bg-gradient-to-r from-blue-500 to-green-800",
+    Water = "bg-gradient-to-r from-blue-500 to-blue-800",
 }
+
 
 export type CollectionColor = keyof typeof CollectionColors;
